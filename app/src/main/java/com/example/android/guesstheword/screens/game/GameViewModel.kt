eb38858accd758470ae266479ae3b9d8ba4c64a6
Yaowei Ma <yaoweima@google.com>
2023-01-18 14:28:19 -0800

    encapsulate livedata by private mutableLiveData + exposing read-only LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,14 +1,23 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+  // The livedata should be used as mutable inside ViewModel, but expose as immutable LiveData
   // The current word
-  var word = MutableLiveData<String>()
+  private val _word = MutableLiveData<String>()
+  val word: LiveData<String>
+    get() = _word
   // The current score
-  var score = MutableLiveData<Int>()
+  // The current score
+  private val _score = MutableLiveData<Int>()
+  val score: LiveData<Int>
+    get() {
+      return _score
+    }
 
   // The list of words - the front of the list is the next word to guess
   private lateinit var wordList: MutableList<String>
@@ -44,8 +53,8 @@ class GameViewModel : ViewModel() {
   }
 
   init {
-    word.value = ""
-    score.value = 0
+    _word.value = ""
+    _score.value = 0
     resetList()
     nextWord()
     Log.i("GameViewModel", "GameViewModel created!")
@@ -60,12 +69,12 @@ class GameViewModel : ViewModel() {
   /** Methods for buttons presses **/
   // update score and word (both will be in viewmodel)
   fun onSkip() {
-    score.value = score.value?.minus(1)
+    _score.value = _score.value?.minus(1)
     nextWord()
   }
 
   fun onCorrect() {
-    score.value = score.value?.plus(1)
+    _score.value = _score.value?.plus(1)
     nextWord()
   }
 
@@ -75,7 +84,7 @@ class GameViewModel : ViewModel() {
   private fun nextWord() {
     if (!wordList.isEmpty()) {
       //Select and remove a word from the list
-      word.value = wordList.removeAt(0)
+      _word.value = wordList.removeAt(0)
     }
     // don't do the UI update in viewModel.
     // updateWordText()
